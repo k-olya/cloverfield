1e2fea7680c4b30bbe95f9f6d7e5c5ecb51825f8
Nikolay Chistyakov <cats@feel.so>
2023-02-13 20:05:28 +0300

    [+] fix tests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,17 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom/extend-expect";
+
+global.DOMRect = class DOMRect {
+        bottom:number=0;
+        left:number=0;
+        right:number=0;
+        top:number=0;
+        constructor (public x=0, public y=0, public width=0, public height=0) {};
+        static fromRect(other?: DOMRectInit): DOMRect {
+            return other ? new DOMRect(other.x,other.y,other.width,other.height) : new DOMRect();
+        }
+        toJSON() {
+            return JSON.stringify(this)
+        }
+    }
