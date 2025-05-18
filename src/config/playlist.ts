c5d9760183d5d00de2f001193d7002d352af2223
kolay <cats@feel.so>
2025-05-18 18:49:20 +0300

    [+] add playlist

---
+++ src/config/playlist.ts
@@ -0,0 +1,17 @@
+export const PLAYLIST = `003_3._Home.aac
+006_6._Moonlight.aac
+009_9._Voyager.aac
+012_12._Astronomy.aac
+015_15._Leaving_at_Dawn.aac
+018_18._Headlights_in_the_Fog.aac
+021_21._The_Saga.aac
+024_24._New_Beginnings.aac
+027_27._Digital_Sunset.aac
+030_30._Arcadia.aac
+033_33._Afterglow.aac
+036_36._Night_Crawler.aac
+039_39._End_Credits.aac
+040_40._Miami_Sky.aac
+042_42._Sycophant.aac
+045_45._Agent_Cooper.aac
+`.split("\n").map(x => x.trim());
